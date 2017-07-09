d0c6b8ed2d34bd21ffd13cd37392cb405aecba90
mandersson1024 <mixand@gmail.com>
2017-07-09 19:51:40 +0200

    Add files via upload

---
+++ Assets/Movie/MoviePlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour
+{
+
+	public void Play () 
+	{
+		Renderer r = GetComponent<Renderer>();
+		MovieTexture movie = (MovieTexture)r.material.mainTexture;
+		movie.Play();
+	}
+
+	public void Stop () 
+	{
+		Renderer r = GetComponent<Renderer>();
+		MovieTexture movie = (MovieTexture)r.material.mainTexture;
+		movie.Stop();
+	}
+
+}
---
+++ Assets/Movie/Sequencer.cs
@@ -0,0 +1,227 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using AlpacaSound.RetroPixelPro;
+
+
+
+public class Sequencer : MonoBehaviour 
+{
+	public GameObject camera_yellowBrown;
+	public GameObject camera_blue;
+	public GameObject camera_monoBlue;
+	public GameObject camera_blueRed;
+	public GameObject camera_green;
+	public GameObject camera_laughter;
+
+	public GameObject field;
+	public GameObject flower;
+	public GameObject wave;
+	public GameObject laughter;
+
+	RetroPixelPro blueRedPixel;
+	//RetroPixelPro yellowPixel;
+	RetroPixelPro laughterPixel;
+
+	void Start ()
+	{
+		camera_yellowBrown = GameObject.Find("Camera - Yellow Brown");
+		camera_blueRed = GameObject.Find("Camera - Blue Red");
+		camera_green = GameObject.Find("Camera - MonoGreen");
+		camera_monoBlue = GameObject.Find("Camera - MonoBlue");
+		camera_blue = GameObject.Find("Camera - Blue");
+		camera_laughter = GameObject.Find("Camera - Laughter");
+
+		field = GameObject.Find("Quad - Field");
+		flower = GameObject.Find("Quad - Flower");
+		wave = GameObject.Find("Quad - Movie");
+		laughter = GameObject.Find("Quad - Laughter");
+
+		SetCameraEnabled(camera_yellowBrown, false);
+		SetCameraEnabled(camera_blue, false);
+		SetCameraEnabled(camera_blueRed, false);
+		SetCameraEnabled(camera_green, false);
+		SetCameraEnabled(camera_laughter, false);
+
+		SetRendererEnabled(field, false);
+		SetRendererEnabled(flower, false);
+		SetRendererEnabled(wave, false);
+		SetRendererEnabled(laughter, false);
+
+		blueRedPixel = camera_blueRed.GetComponent<RetroPixelPro>();
+		//yellowPixel = camera_yellowBrown.GetComponent<RetroPixelPro>();
+		laughterPixel = camera_laughter.GetComponent<RetroPixelPro>();
+
+		StartCoroutine(Phase1());
+	}
+
+
+	void SetCameraEnabled(GameObject obj, bool b)
+	{
+		obj.GetComponent<Camera>().enabled = b;
+	}
+	
+	
+	void SetRendererEnabled(GameObject obj, bool b)
+	{
+		obj.GetComponent<MeshRenderer>().enabled = b;
+	}
+	
+	
+	IEnumerator Phase1()
+	{
+		SetRendererEnabled(flower, true);
+		SetCameraEnabled(camera_blueRed, true);
+
+		blueRedPixel.horizontalResolution = 192;
+		blueRedPixel.verticalResolution = 108;
+
+		float startTime = Time.time;
+
+		float initialDuration = 8;
+		while(Time.time < startTime + initialDuration)
+		{
+			float duration  = Time.time - startTime;
+			flower.transform.localScale = new Vector3(2048, 1580, 1) * 0.01f * duration / 2;
+			flower.transform.Rotate(Vector3.forward, -3 * Time.deltaTime);
+
+			yield return null;
+		}
+
+		float pixelFadeDuration = 3;
+
+		while(Time.time < startTime + initialDuration + pixelFadeDuration)
+		{
+			float duration  = Time.time - startTime;
+			float currentDuration = duration - initialDuration;
+			flower.transform.localScale = new Vector3(2048, 1580, 1) * 0.01f * duration / 2;
+			flower.transform.Rotate(Vector3.forward, -3 * Time.deltaTime);
+			
+			blueRedPixel.horizontalResolution = (int) Mathf.Lerp(192, 1920, currentDuration / pixelFadeDuration);
+			blueRedPixel.verticalResolution = (int) Mathf.Lerp(108, 1080, currentDuration / pixelFadeDuration);
+			
+			yield return null;
+		}
+
+		blueRedPixel.horizontalResolution = 1920;
+		blueRedPixel.verticalResolution = 1080;
+
+		while(Time.time < startTime + 14)
+		{
+			float duration  = Time.time - startTime;
+			flower.transform.localScale = new Vector3(2048, 1580, 1) * 0.01f * duration / 2;
+			flower.transform.Rotate(Vector3.forward, -3 * Time.deltaTime);
+			yield return null;
+		}
+		
+		SetRendererEnabled(flower, false);
+		SetCameraEnabled(camera_blueRed, false);
+
+		StartCoroutine(Phase2());
+	}
+	
+	
+	IEnumerator Phase2()
+	{
+		SetRendererEnabled(wave, true);
+		SetCameraEnabled(camera_monoBlue, true);
+		wave.GetComponent<MoviePlayer>().Play();
+
+		float startTime = Time.time;
+		while(Time.time < startTime + 8)
+		{
+			yield return null;
+		}
+		
+		SetCameraEnabled(camera_monoBlue, false);
+		SetCameraEnabled(camera_blue, true);
+		
+		while(Time.time < startTime + 13)
+		{
+			yield return null;
+		}
+		
+		SetCameraEnabled(camera_blue, false);
+		SetCameraEnabled(camera_green, true);
+		
+		while(Time.time < startTime + 20)
+		{
+			yield return null;
+		}
+		
+		SetRendererEnabled(wave, false);
+		SetCameraEnabled(camera_green, false);
+		wave.GetComponent<MoviePlayer>().Stop();
+
+		StartCoroutine(Phase3());
+	}
+	
+	
+	IEnumerator Phase3()
+	{
+		SetRendererEnabled(field, true);
+		SetCameraEnabled(camera_yellowBrown, true);
+		
+		Vector3 startPosition = new Vector3(-2, 10, 10);
+		Vector3 endPosition = new Vector3(5, 12, 10);
+		
+		float duration = 10;
+		
+		float startTime = Time.time;
+		while(Time.time < startTime + duration)
+		{
+			float currentTime = Time.time - startTime;
+			field.transform.localPosition = Vector3.Lerp(startPosition, endPosition, currentTime/duration);
+			yield return null;
+		}
+		
+		
+		SetRendererEnabled(field, false);
+		SetCameraEnabled(camera_yellowBrown, false);
+		
+		StartCoroutine(Phase4());
+	}
+	
+	
+	IEnumerator Phase4()
+	{
+		SetRendererEnabled(laughter, true);
+		SetCameraEnabled(camera_laughter, true);
+		
+		laughterPixel.horizontalResolution = 160;
+		laughterPixel.verticalResolution = 100;
+		laughterPixel.strength = 0;
+		
+		float startTime = Time.time;
+
+		float duration1 = 3;
+		float duration2 = 4;
+		float duration3 = 7;
+
+		while(Time.time < startTime + duration1)
+		{
+			yield return null;
+		}
+
+		laughterPixel.strength = 1;
+
+		while(Time.time < startTime + duration1 + duration2)
+		{
+			yield return null;
+		}
+		
+		laughterPixel.horizontalResolution = 1920;
+		laughterPixel.verticalResolution = 1080;
+		laughterPixel.strength = 0.56f;
+
+		while(Time.time < startTime + duration1 + duration2 + duration3)
+		{
+			yield return null;
+		}
+		
+		SetRendererEnabled(laughter, false);
+		SetCameraEnabled(camera_laughter, false);
+	}
+	
+	
+}
