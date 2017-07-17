546ee7122b23f293525f9ef82470c9aa2eaa75df
mandersson1024 <mixand@gmail.com>
2017-07-17 12:06:20 +0200

    Created ColormapPresetEditor

---
+++ Assets/Retro Pixel Pro/Scripts/Editor/ColormapPresetEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AlpacaSound.RetroPixelPro
+{
+
+    [CustomEditor(typeof(ColormapPreset))]
+    public class ColormapPresetEditor : Editor
+    {
+        ColormapPreset _target;
+
+        const string MENU_ITEM_NAME = "Create New Colormap";
+
+        void OnEnable()
+        {
+            _target = target as ColormapPreset;
+        }
+
+
+        public override void OnInspectorGUI()
+        {
+            DrawColors();
+        }
+
+
+        void DrawColors()
+        {
+            for (int i = 0; i < _target.palette.Length; i += 5)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                for (int j = 0; j < 5; ++j)
+                {
+                    if (i + j < _target.palette.Length)
+                    {
+                        Color color = _target.palette[i + j];
+                        EditorGUILayout.ColorField(GUIContent.none, color, false, false, false, null, GUILayout.Width(40), GUILayout.Height(25));
+                    }
+                    else
+                    {
+                        GUILayout.Space(46);
+                    }
+
+                    //EditorGUILayout.Space();
+                }
+
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.Space();
+            }
+        }
+    }
+
+}
+
+
