9a7b2e522f0044adf2c63957cad9ce2db9476444
mandersson1024 <mixand@gmail.com>
2017-07-19 18:28:02 +0200

    Mac build on PC

--- Assets/Movie/MoviePlayer.cs
+++ Assets/Movie/MoviePlayer.cs
@@ -6,16 +6,20 @@ public class MoviePlayer : MonoBehaviour
 
 	public void Play () 
 	{
-		Renderer r = GetComponent<Renderer>();
+#if !UNITY_WEBGL
+        Renderer r = GetComponent<Renderer>();
 		MovieTexture movie = (MovieTexture)r.material.mainTexture;
 		movie.Play();
+#endif
 	}
 
 	public void Stop () 
 	{
+#if !UNITY_WEBGL
 		Renderer r = GetComponent<Renderer>();
 		MovieTexture movie = (MovieTexture)r.material.mainTexture;
 		movie.Stop();
-	}
+#endif
+    }
 
 }
