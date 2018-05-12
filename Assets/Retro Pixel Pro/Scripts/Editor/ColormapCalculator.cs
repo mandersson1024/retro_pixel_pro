c2b3daf7d27d19a7fb264cb56e2d3a1da5e5c564
mandersson1024 <mixand@gmail.com>
2018-05-12 22:33:29 +0200

    New class ColormapCalculatorProgress

--- Assets/Retro Pixel Pro/Scripts/Editor/ColormapCalculator.cs
+++ Assets/Retro Pixel Pro/Scripts/Editor/ColormapCalculator.cs
@@ -11,33 +11,24 @@ namespace AlpacaSound.RetroPixelPro
 		public float progress;
 		public Color32[] pixelBuffer;
 
-		ColormapPrecision precision;
 		Color32[] palette;
 		bool[] usedColors;
 		int numColors;
 		System.Action doneCallback;
 		int colorsteps;
 		int totalPixels;
-		Color32 colorProgress;
-
+		ColormapCalculatorProgress ccprogress;
 
 
 		public ColormapCalculator(ColormapPrecision precision, Color32[] palette, bool[] usedColors, int numColors, System.Action doneCallback)
 		{
-			this.precision = precision;
 			this.palette = palette;
 			this.usedColors = usedColors;
 			this.doneCallback = doneCallback;
 			this.numColors = numColors;
 			progress = 0;
-			colorProgress = new Color32(0, 0, 0, 0);
-			SetupPixelBuffer();
-		}
-
-
-		void SetupPixelBuffer()
-		{
 			colorsteps = ColormapUtils.GetPrecisionColorsteps(precision);
+			ccprogress = new ColormapCalculatorProgress(colorsteps);
 			totalPixels = colorsteps * colorsteps * colorsteps;
 			pixelBuffer = new Color32[totalPixels];
 		}
@@ -53,39 +44,15 @@ namespace AlpacaSound.RetroPixelPro
 			}
 		}
 
-		private void gotoNextPixel()
-		{
-			++colorProgress.r;
-
-			if (colorProgress.r == colorsteps)
-			{
-				colorProgress.r = 0;
-				++colorProgress.g;
-
-				if (colorProgress.g == colorsteps)
-				{
-					colorProgress.g = 0;
-					++colorProgress.b;
-				}
-			}
-		}
-
-		private int getPixelProgress()
-		{
-			return colorProgress.r + colorProgress.g * colorsteps + colorProgress.b * colorsteps * colorsteps;
-		}
-
 
 		void CalculateNextPixel()
 		{
-			int pixelProgress = getPixelProgress();
-
-			if (pixelProgress < totalPixels)
+			if (ccprogress.progress < totalPixels)
 			{
 				byte paletteIndex = GetClosestPaletteIndex();
-				pixelBuffer[pixelProgress] = new Color32(0, 0, 0, paletteIndex);
-				gotoNextPixel();
-				progress = (float)pixelProgress / (float)totalPixels;
+				pixelBuffer[ccprogress.progress] = new Color32(0, 0, 0, paletteIndex);
+				ccprogress.NextPixel();
+				progress = (float)ccprogress.progress / (float)totalPixels;
 			}
 			else
 			{
@@ -98,7 +65,7 @@ namespace AlpacaSound.RetroPixelPro
 		{
 			float closestDistance = float.MaxValue;
 			int closestIndex = 0;
-			Vector3 rgb = new Vector3(colorProgress.r, colorProgress.g, colorProgress.b);
+			Vector3 rgb = ccprogress.GetRGBCoordinate();
 			rgb = 256 * rgb / (colorsteps - 1);
 
 			for (int i = 0; i < numColors; ++i)
---
+++ Assets/Retro Pixel Pro/Scripts/Editor/ColormapCalculatorProgress.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlpacaSound.RetroPixelPro
+{
+
+	public class ColormapCalculatorProgress
+	{
+		public Color32 color;
+		public int steps;
+		public int progress;
+
+		public ColormapCalculatorProgress(int colorsteps)
+		{
+			steps = colorsteps;
+			color = new Color32(0, 0, 0, 0);
+			progress = 0;
+		}
+
+		public void NextPixel()
+		{
+			++color.r;
+
+			if (color.r == steps)
+			{
+				color.r = 0;
+				++color.g;
+
+				if (color.g == steps)
+				{
+					color.g = 0;
+					++color.b;
+				}
+			}
+
+			++progress;
+		}
+
+		public Vector3 GetRGBCoordinate()
+		{
+			return new Vector3(color.r, color.g, color.b);
+		}
+
+
+	}
+
+}
+
