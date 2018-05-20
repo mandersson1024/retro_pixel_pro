f421d6c9abef95575bb7f7907bb76b7306869068
mandersson1024 <mixand@gmail.com>
2018-05-20 15:16:31 +0200

    Working on unit tests for finding point on line.

--- Assets/Retro Pixel Pro/Scripts/ColormapUtils.cs
+++ Assets/Retro Pixel Pro/Scripts/ColormapUtils.cs
@@ -50,19 +50,22 @@ namespace AlpacaSound.RetroPixelPro
 		static public Vector3 GetColorstepPosition(Color32 color, int colorsteps)
 		{
 			int stepLength = 256 / colorsteps;
-
 			float offset = stepLength / 2f;
 
-			Vector3 v = new Vector3();
-
-			v.x = ((color.r / stepLength) * stepLength + offset) / 256f;
-			v.y = ((color.g / stepLength) * stepLength + offset) / 256f;
-			v.z = ((color.b / stepLength) * stepLength + offset) / 256f;
-
-
-			//xyz != 0 returnerar vektor 0
+			Vector3 v = new Vector3
+			{
+				x = ((color.r / stepLength) * stepLength + offset) / 256f,
+				y = ((color.g / stepLength) * stepLength + offset) / 256f,
+				z = ((color.b / stepLength) * stepLength + offset) / 256f
+			};
 
 			return v;
+		}
+
+
+		static public Vector3 FindNearestPointOnLine(Vector3 lineStart, Vector3 lineEnd, Vector3 otherPoint)
+		{
+			return Vector3.zero;
 		}
 
 
---
+++ Assets/Testing/Editor/Unit Tests/ColormapValueTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+using AlpacaSound.RetroPixelPro;
+
+public class ColormapValueTest
+{
+
+	[Test]
+	public void NewTestScriptSimplePasses()
+	{
+		Assert.AreEqual(new Vector3(0.5f, 0, 0), ColormapUtils.FindNearestPointOnLine(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0.5f, 1, 0)));
+	}
+}
